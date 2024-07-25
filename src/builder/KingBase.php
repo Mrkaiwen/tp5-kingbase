fd0311e4a57f5566b7fd68d5736bc5cfce5980bc
yaok <ASD123%yaok@0>
2024-07-25 17:34:23 +0800

    init

--- src/builder/KingBase.php
+++ src/builder/KingBase.php
@@ -56,17 +56,68 @@ class KingBase extends Builder
 
 
     /**
-     * 字段名分析
+     * 字段和表名处理
      * @access public
-     * @param  Query  $query    查询对象
-     * @param  mixed  $key      字段名
-     * @param  bool   $strict   严格检测
+     * @param  Query     $query 查询对象
+     * @param  mixed     $key   字段名
+     * @param  bool      $strict   严格检测
      * @return string
      */
     public function parseKey(Query $query, $key, $strict = false)
     {
-        return $key instanceof Expression ? $key->getValue() : $key;
+        if (is_numeric($key)) {
+            return $key;
+        } elseif ($key instanceof Expression) {
+            return $key->getValue();
+        }
+
+        $key = trim($key);
+
+        if(strpos($key, '->>') && false === strpos($key, '(')){
+            // JSON字段支持
+            list($field, $name) = explode('->>', $key, 2);
+
+            return $this->parseKey($query, $field, true) . '->>\'$' . (strpos($name, '[') === 0 ? '' : '.') . str_replace('->>', '.', $name) . '\'';
+        }
+        elseif (strpos($key, '->') && false === strpos($key, '(')) {
+            // JSON字段支持
+            list($field, $name) = explode('->', $key, 2);
+
+            return 'json_extract(' . $this->parseKey($query, $field, true) . ', \'$' . (strpos($name, '[') === 0 ? '' : '.') . str_replace('->', '.', $name) . '\')';
+        } elseif (strpos($key, '.') && !preg_match('/[,\'\"\(\)`\s]/', $key)) {
+            list($table, $key) = explode('.', $key, 2);
+
+            $alias = $query->getOptions('alias');
+
+            if ('__TABLE__' == $table) {
+                $table = $query->getOptions('table');
+                $table = is_array($table) ? array_shift($table) : $table;
+            }
+
+            if (isset($alias[$table])) {
+                $table = $alias[$table];
+            }
+        }
+
+        if ($strict && !preg_match('/^[\w\.\*]+$/', $key)) {
+            throw new \think\Exception('not support data:' . $key);
+        }
+
+        if ('*' != $key && !preg_match('/[,\'\"\*\(\)`.\s]/', $key)) {
+            $key = '`' . $key . '`';
+        }
+
+        if (isset($table)) {
+            if (strpos($table, '.')) {
+                $table = str_replace('.', '`.`', $table);
+            }
+
+            $key = '`' . $table . '`.' . $key;
+        }
+
+        return $key;
     }
+
 
 
     /**
@@ -250,7 +301,7 @@ class KingBase extends Builder
             [
                 $replace ? 'REPLACE' : 'INSERT',
                 $this->parseTable($query, $options['table']),
-                '`'.implode('` ,`', $fields).'`',
+                implode(' , ', $fields),
                 implode(' , ', $values),
                 $this->parseComment($query, $options['comment']),
             ],
