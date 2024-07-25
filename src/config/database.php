59048a6671d6c35a85646706ad53448fa83c1384
steven <781422696@qq.com>
2024-07-25 15:30:19 +0800

    Add files via upload

--- src/config/database.php
+++ src/config/database.php
@@ -1,63 +1,54 @@
 <?php
 
 return [
-    // 默认使用的数据库连接配置
-    'default'         => 'mysql',
-
-    // 自定义时间查询规则
-    'time_query_rule' => [],
-
+    // 数据库类型
+    'type'              => 'Wwws3\Kingbase\connector\KingBase',
+    // 服务器地址
+    'hostname'        => $host,
+    // 数据库名
+    'database'        => $database,
+    // 用户名
+    'username'        => $user,
+    // 密码
+    'password'        => $password,
+    // 端口
+    'hostport'        => $port,
+    // 连接dsn
+    'dsn'             => '',
+    // 数据库连接参数
+    'params'          => [],
+    // 数据库编码默认采用utf8
+    'charset'         => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'          => 'wwws3_video.',
+    // 数据库调试模式
+    'debug'           => $debug,
+    // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+    'deploy'          => $deploy,
+    // 数据库读写是否分离 主从式有效
+    'rw_separate'     => $rw_separate,
+    // 读写分离后 主服务器数量
+    'master_num'      => 1,
+    // 指定从服务器序号
+    'slave_no'        => '',
+    // 自动读取主库数据
+    'read_master'     => false,
+    // 是否严格检查字段是否存在
+    'fields_strict'   => true,
+    // 数据集返回类型
+    'resultset_type'  => 'collection',
     // 自动写入时间戳字段
-    // true为自动识别类型 false关闭
-    // 字符串则明确指定时间字段类型 支持 int timestamp datetime date
     'auto_timestamp'  => false,
-
     // 时间字段取出后的默认时间格式
-    'datetime_format' => 'Y-m-d H:i:s',
-
-    // 数据库连接配置信息
-    'connections'     => [
-        'mysql' => [
-            // 数据库类型
-            'type'              => 'Wwws3\Kingbase\connector\KingBase',
-            // 查询类型
-            'builder'             => 'Wwws3\Kingbase\builder\KingBase',
-            // 服务器地址
-            'hostname'          => 'localhost',
-            // 数据库名
-            'database'          => 'test',
-            // 用户名
-            'username'          => 'SYSTEM',
-            // 密码
-            'password'          => 'KingBase',
-            // 端口
-            'hostport'          => '54321',
-            // 数据库连接参数
-            'params'            => [],
-            // 数据库编码默认采用utf8
-            'charset'           => '',
-            // 数据库表前缀
-            'prefix'            => '',
-            // 开启自动主库读取
-            'read_master'       => false,
-            // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-            'deploy'            => 0,
-            // 数据库读写是否分离 主从式有效
-            'rw_separate'       => 0,
-            // 读写分离后 主服务器数量
-            'master_num'        => 1,
-            // 指定从服务器序号
-            'slave_no'          => '',
-            // 是否严格检查字段是否存在
-            'fields_strict'     => true,
-            // 是否需要断线重连
-            'break_reconnect'   => false,
-            // 监听SQL
-            'trigger_sql'       => true,
-            // 开启字段缓存
-            'fields_cache'      => false,
-            // 字段缓存路径
-            'schema_cache_path' => '',
-        ],
-    ],
+    'datetime_format' => false,
+    // 是否需要进行SQL性能分析
+    'sql_explain'     => false,
+    // Builder类
+    'builder'         => 'Wwws3\Kingbase\builder\KingBase',
+    // Query类
+    'query'           => '\\think\\db\\Query',
+    // 是否需要断线重连
+    'break_reconnect' => false,
+    // 断线标识字符串
+    'break_match_str' => [],
 ];
